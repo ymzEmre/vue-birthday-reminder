413b56eb848ed5ba4da00c393ecc1fc814e4b060
ymzEmre <dev.yilmazemre@gmail.com>
2022-02-12 05:40:18 +0300

    Add form validation to the customer update

--- src/components/CustomerUpdate.vue
+++ src/components/CustomerUpdate.vue
@@ -9,7 +9,7 @@ defineProps({
   },
 });
 
-const { displayModal, selectedCity, cities, updatedata, openModal, closeModal, updateUser, fetchCustomers } = customerUpdate();
+const { displayModal, selectedCity, cities, updatedata, openModal, closeModal, submitted, v$, handleSubmit } = customerUpdate();
 </script>
 
 <template>
@@ -20,32 +20,33 @@ const { displayModal, selectedCity, cities, updatedata, openModal, closeModal, u
       @click="openModal(user._id, user.name, user.birthday, user.group)"
     ></Button>
     <Dialog header="Update" v-model:visible="displayModal" :style="{ width: '20vw' }" :modal="true">
-      <span class="p-float-label p-mt-4">
-        <InputText id="username2" class="customer-name" type="text" v-model="updatedata.name" />
-        <label for="username">Username</label>
-      </span>
-
-      <div class="p-field p-col-12 customer-birthday">
-        <label for="date">Date</label>
-        <InputMask mask="9999/99/99" v-model="updatedata.birthday" placeholder="____/__/__" slotChar="yyyy/mm/dd" />
-      </div>
-
-      <label for="date">Group</label>
-      <Listbox class="p-mt-2" v-model="selectedCity" @input="updatedata.group" :options="cities" optionLabel="name" style="width: 21rem" />
-      <template #footer>
-        <Button label="Cancel" icon="pi pi-times" @click="closeModal" class="p-button-text" />
-
-        <Button
-          class="p-button-success"
-          label="Update"
-          icon="pi pi-check"
-          @click="
-            updateUser();
-            fetchCustomers();
-          "
-          autofocus
-        />
-      </template>
+      <form @submit.prevent="handleSubmit(!v$.$invalid)">
+        <span class="p-float-label p-mt-4">
+          <InputText id="name" v-model="v$.name.$model" class="customer-name" :class="{ 'p-invalid': v$.name.$invalid && submitted }" />
+          <label for="name" :class="{ 'p-error': v$.name.$invalid && submitted }">Name</label>
+        </span>
+        <span v-if="v$.name.$error && submitted">
+          <span id="email-error" v-for="(error, index) of v$.name.$errors" :key="index">
+            <small class="p-error">{{ error.$message }}</small>
+          </span>
+        </span>
+        <small v-if="(v$.name.$invalid && submitted) || v$.name.$pending.$response" class="p-error">{{
+          v$.name.required.$message.replace("Value", "Name")
+        }}</small>
+        <div class="p-field p-col-12 customer-birthday">
+          <label for="birthday" :class="{ 'p-error': v$.birthday.$invalid && submitted }">Birthday</label>
+          <InputMask id="birthday" mask="9999/99/99" v-model="v$.birthday.$model" placeholder="____/__/__" slotChar="yyyy/mm/dd" />
+          <small v-if="(v$.birthday.$invalid && submitted) || v$.birthday.$pending.$response" class="p-error">{{
+            v$.birthday.required.$message.replace("Value", "Birthday")
+          }}</small>
+        </div>
+        <label for="group">Group</label>
+        <Listbox class="p-mt-2" v-model="selectedCity" @input="updatedata.group" :options="cities" optionLabel="name" style="width: 21rem" />
+        <div class="p-mt-6 modal-footer">
+          <Button label="Cancel" icon="pi pi-times" @click="closeModal" class="p-button-text" />
+          <Button type="submit" class="p-button-success" label="Update" icon="pi pi-check" />
+        </div>
+      </form>
     </Dialog>
   </div>
 </template>
@@ -66,5 +67,11 @@ const { displayModal, selectedCity, cities, updatedata, openModal, closeModal, u
 
 .p-inputmask {
   width: 100%;
+}
+
+.modal-footer {
+  display: flex;
+  justify-content: end;
+  margin-bottom: -1em;
 }
 </style>
